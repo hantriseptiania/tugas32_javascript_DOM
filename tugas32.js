5510785ff17f5ef78f14104925c906a5b511672e
hantriseptiania <71365751+hantriseptiania@users.noreply.github.com>
2020-09-22 09:24:43 +0700

    Add files via upload

---
+++ tugas32.js
@@ -0,0 +1,9 @@
+function panggilEksternal(){
+	var namadepan = document.getElementByID('namadepan')
+	var namabelakang = document.getElementByID('namabelakang')
+	var hobi = document.getElementByID ('hobi')
+
+	console.log(namadepan)
+	console.log(namabelakang)
+	console.log(hobi)
+}
